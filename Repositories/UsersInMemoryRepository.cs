0549c68e77bc33939b4406f49398e9c97fef1149
jvelazquez99 <jocavefe1999@gmail.com>
2022-11-15 22:04:43 -0300

    Jose Velazquez Tercer Parcial

---
+++ Repositories/UsersInMemoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using PrimerParcial.Models;
+using PrimerParcial.Interfaces;
+
+
+namespace PrimerParcial.Repositories
+{
+    public class UsersInMemoryRepository: IUserRepository
+    {
+        private readonly List<User> _users = new List<User>
+        {
+            new()
+            {
+                Id = 1,
+                Fullname = "Jose Velazquez",
+                Username = "jvelazquez",
+                Password = "123456",
+            }
+        };
+        public async Task<User?> GetUserByCredentials(string username, string password)
+        {
+            return _users.FirstOrDefault(x => x.Username.Equals(username) && x.Password.Equals(password));
+        }
+    }
+}
